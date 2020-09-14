67339a9c90c3e4a4c41d7fa9c33df5c1327acc07
Brandan Schmitz <brandan.schmitz@intelletive.com>
2020-09-14 14:25:00 -0500

    fix(*): initial commit

---
+++ lib/markedit_api.dart
@@ -0,0 +1,12 @@
+/// markedit_api
+///
+/// A Aqueduct web server.
+library markedit_api;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+export 'package:aqueduct/managed_auth.dart';
+
+export 'channel.dart';
